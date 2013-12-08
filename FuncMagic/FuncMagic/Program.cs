bb02cda3c77e591762875b44679637adf274ed1f
abhijit gadkari <abhigad@hotmail.com>
2013-12-07 23:36:36 -0800

    included funcmagic
    this is a test to learn git source code management system

---
+++ FuncMagic/FuncMagic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FuncMagic
+{
+    //A delegate is a reference type that can be used to encapsulate a named or an anonymous method. Delegates are similar to function pointers in C++; however, delegates are type-safe and secure.
+
+    //following is an example of the generic delegate
+    internal delegate T Transform<T>(T args);
+
+    internal class Program
+    {
+        public readonly int x = 5;
+
+        private static void Main()
+        {
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
